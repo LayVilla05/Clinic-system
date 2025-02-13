c83925239c06289e9e5ad939ea5c9aed2b2898ae
LayVilla05 <layvilla05@gmail.com>
2025-02-13 20:24:43 +0700

    add model

--- clinic/Model/PatientServices.cs
+++ clinic/Model/PatientServices.cs
@@ -8,6 +8,16 @@ namespace clinic.Model
 {
     public class PatientServices
     {
+        public int PatientService { get; set; }
+        public int PatientsId { get; set; }
+        public int ServiceId { get; set; }
+        public int DoctorId { get; set; }
+        public DateTime Date { get; set; }
+        public string  Status { get; set; }
+        public Decimal Price { get; set; }
+        public string  PatientName { get; set; }
+        public string ServiceName { get; set; }
+        public string DoctorName { get; set; }
 
     }
 }
